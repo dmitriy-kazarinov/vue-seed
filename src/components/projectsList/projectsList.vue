b80c6285ae084e1b52948262d379ef9811860256
 Dmitry <dmitry@Dmitrys-MacBook-Pro.local>
2017-05-25 22:59:39 +0300

    Updated home component, added links, removed e2e tests, updated unit tests, added new dependency

--- src/components/projectsList/projectsList.vue
+++ src/components/projectsList/projectsList.vue
@@ -1,23 +1,24 @@
 <style src="./projectsList.css"></style>
 <script src="./projectsList.js"></script>
 <template>
-
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Project Name</th>
-        <th>Completed</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="project in projects"
-        :class="[project.completed ? 'completed' : 'uncompleted', 'project-row']"
-        @click="goToProject(project.id)">
-        <td>{{project.title}}</td>
-        <td>{{project.completed ? 'Yes' : 'No'}}</td>
-      </tr>
-    </tbody>
-  </table>
-
+  <div>
+    <h2>Projects table</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Project Name</th>
+          <th>Completed</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="project in projects"
+          :class="[project.completed ? 'completed' : 'uncompleted', 'project-row']"
+          @click="goToProject(project.id)">
+          <td>{{project.title}}</td>
+          <td>{{project.completed ? 'Yes' : 'No'}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
